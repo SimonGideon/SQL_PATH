c23e2f6bcb5757c9a974b1a26341ea609e232f03
SimonGideon <simongideon918@gmail.com>
2021-06-06 16:31:25 +0300

    Completion

--- Assignment_1.sql
+++ Assignment_1.sql
@@ -18,3 +18,44 @@ VALUES
 
 
 select * from Oil_marketers
+create Table Products
+(
+[PRDT_Name] VarChar(54), /*PRDT stands for productt*/
+[Supplier] VarChar(32),
+[PRDT_Price] Numeric(38)
+
+)
+
+
+insert into Products(PRDT_Name, Supplier, PRDT_Price) 
+Values
+('KEROSINE', 'KENOL KOBIL Co', '100'),
+('DISEL', 'SHELL Ke', '150'),
+('PETROL', 'TOTAL Ke', '200')
+
+
+select * from Products
+
+create Table Orders
+(
+[ORDER_No] INT IDENTITY(354657, 545465) PRIMARY KEY,
+[BUYER] VarChar(32),
+[PRODUCT] VarChar(38),
+[LITERS] Numeric(38)
+)
+
+
+insert into Orders(BUYER, PRODUCT, LITERS) 
+Values
+('SYOKIMAU FLUIDS', 'KEROSINE', '10000'),
+('MOMBASA MOTORS', 'DISEL', '15000'),
+('CMC Motors', 'PETROL', '20000'),
+('Marshalls', 'PETROL', '30000'),
+('Motor Hub Limited', 'PETROL', '35700'),
+('TATA Kenya', 'DISEL', '230000'),
+('ISUZU Kenya', 'PETROL', '350000'),
+('ISUZU', 'DISEL', '450000'),
+('INeax Motor', 'PETROL', '200000'),
+('KENPOL', 'KEROSINE', '45000')
+
+select * from Orders
